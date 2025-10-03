6ff81aacad64a7c66f63a433615585ffdd4a71ea
len3173 <2025-1-00340@lpu.edu.ph>
2025-10-03 08:08:57 +0800

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        printf("Inohiaban Lenny H.\n");
+    }
+    return 0;
+}
